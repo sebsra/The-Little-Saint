a45dacf228cedb945178a5097ede516adccc75e7
sebsra <sebastian.engbert@gmail.com>
2025-04-13 19:03:56 +0200

    Vorbereitung Level 1 für Studienarbeit

---
+++ scenes/levels/prince_levels/game_prince.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+func _ready() -> void:
+	# Verbinde mit dem player_died Signal aus dem Global Autoload in Godot 4.4 Syntax
+	Global.player_died.connect(_on_player_died)
+
+# Diese Funktion wird aufgerufen, wenn die set_back_zone betreten wird
+func _on_set_back_zone_body_entered(body: Node2D) -> void:
+	$Player.position = Vector2(100, -100)  # Replace with your desired coordinates
+	GlobalHUD.change_life(-1)
+
+
+# Diese Funktion wird aufgerufen, wenn das player_died Signal emittiert wird
+func _on_player_died() -> void:
+	$Player.state_machine.change_state("PlayerDeathState")
+	await get_tree().create_timer(3.0).timeout
+	$Player.position = Vector2(100, -100)
+	$Player.state_machine.change_state("PlayerIdleState")
+	GlobalHUD.reset_to_defaults()
+	
